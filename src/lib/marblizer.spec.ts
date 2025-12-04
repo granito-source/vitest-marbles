ed6212238579e2fc50f33649cbb41030da14260a
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-12-04 06:12:23 -0500

    Redo tests for Marblizer

--- src/lib/marblizer.spec.ts
+++ src/lib/marblizer.spec.ts
@@ -2,62 +2,71 @@ import { SubscriptionLog, TestMessages } from './types';
 import { Marblizer } from './marblizer';
 
 describe('Marblizer', () => {
-  it('converts to marbles TestMessages', () => {
-    // First dash is frame 0
-    // ---(be)----c-f-----|
-    const sample: TestMessages = [
-      { frame: 30, notification: { kind: 'N', value: 'b' } },
-      { frame: 30, notification: { kind: 'N', value: 'e' } },
-      { frame: 110, notification: { kind: 'N', value: 'c' } },
-      { frame: 130, notification: { kind: 'N', value: 'f' } },
-      { frame: 190, notification: { kind: 'C' } },
-    ];
-    const marble = Marblizer.marblize(sample);
+  describe('marblize()', () => {
+    it('converts TestMessages to marbles', () => {
+      const sample: TestMessages = [
+        { frame: 30, notification: { kind: 'N', value: 'b' } },
+        { frame: 30, notification: { kind: 'N', value: 'e' } },
+        { frame: 110, notification: { kind: 'N', value: 'c' } },
+        { frame: 130, notification: { kind: 'N', value: 'f' } }
+      ];
 
-    expect(marble).toEqual('---(be)----c-f-----|');
+      expect(Marblizer.marblize(sample)).toBe('---(be)----c-f');
+    });
+
+    it('recognizes completion frame', () => {
+      const sample: TestMessages = [
+        { frame: 30, notification: { kind: 'N', value: 'b' } },
+        { frame: 110, notification: { kind: 'N', value: 'e' } },
+        { frame: 140, notification: { kind: 'C' } }
+      ];
+
+      expect(Marblizer.marblize(sample)).toBe('---b-------e--|');
+    });
+
+    it('recognizes error frame', () => {
+      const sample: TestMessages = [
+        { frame: 30, notification: { kind: 'N', value: 'b' } },
+        { frame: 30, notification: { kind: 'N', value: 'e' } },
+        { frame: 110, notification: { kind: 'E', error: null } }
+      ];
+
+      expect(Marblizer.marblize(sample)).toBe('---(be)----#');
+    });
+
+    it('handles no emissions without completion', () => {
+      const sample: TestMessages = [];
+
+      expect(Marblizer.marblize(sample)).toBe('');
+    });
+
+    it('handles no emissions with completion', () => {
+      const sample: TestMessages = [{ frame: 110, notification: { kind: 'C' } }];
+
+      expect(Marblizer.marblize(sample)).toBe('-----------|');
+    });
   });
 
-  it('converts to marbles TestMessages with error', () => {
-    const sample: TestMessages = [
-      { frame: 30, notification: { kind: 'N', value: 'b' } },
-      { frame: 30, notification: { kind: 'N', value: 'e' } },
-      { frame: 110, notification: { kind: 'E', error: null } },
-    ];
-    const marble = Marblizer.marblize(sample);
+  describe('marblizeSubscriptions()', () => {
+    it('converts SubscriptionLog without completion to marbles', () => {
+      const logs: SubscriptionLog[] = [{ subscribedFrame: 20, unsubscribedFrame: Infinity }];
 
-    expect(marble).toEqual('---(be)----#');
-  });
+      expect(Marblizer.marblizeSubscriptions(logs)).toEqual(['--^']);
+    });
 
-  it('converts to marbles TestMessages without completion', () => {
-    const sample: TestMessages = [
-      { frame: 30, notification: { kind: 'N', value: 'b' } },
-      { frame: 110, notification: { kind: 'N', value: 'e' } },
-    ];
-    const marble = Marblizer.marblize(sample);
+    it('converts SubscriptionLog with completion to marbles', () => {
+      const logs: SubscriptionLog[] = [{ subscribedFrame: 20, unsubscribedFrame: 80 }];
 
-    expect(marble).toEqual('---b-------e');
-  });
+      expect(Marblizer.marblizeSubscriptions(logs)).toEqual(['--^-----!']);
+    });
 
-  it('converts to marbles Subscriptions without completion', () => {
-    const sample: SubscriptionLog[] = [{ subscribedFrame: 20, unsubscribedFrame: Infinity }];
-    const marble = Marblizer.marblizeSubscriptions(sample);
+    it('handles multiple subscription events', () => {
+      const logs: SubscriptionLog[] = [
+        { subscribedFrame: 30, unsubscribedFrame: 60 },
+        { subscribedFrame: 10, unsubscribedFrame: 50 },
+      ];
 
-    expect(marble).toEqual(['--^']);
-  });
-
-  it('converts to marbles TestMessages without emission (but with completion)', () => {
-    const sample: TestMessages = [{ frame: 110, notification: { kind: 'C' } }];
-    const marble = Marblizer.marblize(sample);
-
-    expect(marble).toEqual('-----------|');
-  });
-
-  it('converts to marbles SubscriptionLogs', () => {
-    const marble = Marblizer.marblizeSubscriptions([
-      { subscribedFrame: 30, unsubscribedFrame: 60 },
-      { subscribedFrame: 10, unsubscribedFrame: 50 },
-    ]);
-
-    expect(marble).toEqual(['---^--!', '-^---!']);
+      expect(Marblizer.marblizeSubscriptions(logs)).toEqual(['---^--!', '-^---!']);
+    });
   });
 });
