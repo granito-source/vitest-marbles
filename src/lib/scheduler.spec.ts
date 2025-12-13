cf2db875bf894b3f020ad836b944eac5e0edcc75
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-12-13 11:46:18 -0500

    Test assertDeepEqual() in Scheduler test
    This way we know how the wrapped TestScheduler is expected to behave.

--- src/lib/scheduler.spec.ts
+++ src/lib/scheduler.spec.ts
@@ -1,12 +1,13 @@
 import { Scheduler } from './scheduler';
 import { TestScheduler } from 'rxjs/testing';
+import { SubscriptionLog, TestMessages } from './types';
 
 describe('Scheduler', () => {
+  beforeEach(() => Scheduler.init());
+
   afterEach(() => Scheduler.reset());
 
   it('provides TestScheduler when initialized', () => {
-    Scheduler.init();
-
     expect(Scheduler.get()).toBeInstanceOf(TestScheduler);
   });
 
@@ -14,5 +15,99 @@ describe('Scheduler', () => {
     Scheduler.reset();
 
     expect(() => Scheduler.get()).toThrow(new Error('Scheduler is not initialized'));
+  });
+
+  describe('TestScheduler#assertDeepEqual()', () => {
+    const subscriptions: SubscriptionLog[] = [
+      { subscribedFrame: 10, unsubscribedFrame: 30 },
+      { subscribedFrame: 50, unsubscribedFrame: Infinity }
+    ];
+    const messages: TestMessages = [
+      { frame: 20, notification: { kind: 'N', value: 'b' } },
+      { frame: 50, notification: { kind: 'N', value: 'e' } }
+    ];
+    let assertDeepEqual: (actual: any, expected: any) => void;
+
+    beforeEach(() => assertDeepEqual = Scheduler.get().assertDeepEqual);
+
+    it('returns normally when expected is undefined', () => {
+      expect(() => assertDeepEqual([], undefined)).not.toThrowError();
+      expect(() => assertDeepEqual(messages, undefined)).not.toThrowError();
+      expect(() => assertDeepEqual(subscriptions, undefined)).not.toThrowError();
+    });
+
+    it('returns normally when expected and actual are empty', () => {
+      expect(() => assertDeepEqual([], [])).not.toThrowError();
+    });
+
+    it('returns normally when the same subscriptions', () => {
+      expect(() => assertDeepEqual(subscriptions, subscriptions)).not.toThrowError();
+    });
+
+    it('throws error when actual subscriptions but expected is empty', () => {
+      expect(() => assertDeepEqual(subscriptions, [])).toThrowError(expect.toSatisfy(e =>
+        /Expected observable to have no subscription points.*But got:.*"-\^-!".*"-----\^"/s.test(e.message)));
+    });
+
+    it('throws error when actual is empty but expected subscriptions', () => {
+      expect(() => assertDeepEqual([], subscriptions)).toThrowError(expect.toSatisfy(e =>
+        /Expected observable to have the following subscription points:.*"-\^-!".*"-----\^".*But got:.*\[]/s.test(e.message)));
+    });
+
+    it('throws error when actual and expected subscriptions have different length', () => {
+      const actual = [
+        { subscribedFrame: 0, unsubscribedFrame: 20 },
+        ...subscriptions
+      ];
+
+      expect(() => assertDeepEqual(actual, subscriptions)).toThrowError(expect.toSatisfy(e =>
+        /Expected observable to have the following subscription points:.*"-\^-!".*"-----\^".*But got:.*"\^-!".*"-\^-!".*"-----\^".*Difference:/s.test(e.message)));
+    });
+
+    it('throws error when actual and expected subscriptions do not match', () => {
+      const actual = [
+        { subscribedFrame: 0, unsubscribedFrame: 20 },
+        { subscribedFrame: 50, unsubscribedFrame: Infinity }
+      ];
+
+      expect(() => assertDeepEqual(actual, subscriptions)).toThrowError(expect.toSatisfy(e =>
+        /Expected observable to have the following subscription points:.*"-\^-!".*"-----\^".*But got:.*"\^-!".*"-----\^".*Difference:/s.test(e.message)));
+    });
+
+    it('returns normally when the same messages', () => {
+      expect(() => assertDeepEqual(messages, messages)).not.toThrowError();
+    });
+
+    it('throws error when actual messages but expected is empty', () => {
+      expect(() => assertDeepEqual(messages, [])).toThrowError(expect.toSatisfy(e =>
+        /Expected notifications to be:.*Array \[].*But got:.*"value": "b".*"value": "e".*Difference:/s.test(e.message)));
+    });
+
+    it('throws error when actual empty but expected messages', () => {
+      expect(() => assertDeepEqual([], messages)).toThrowError(expect.toSatisfy(e =>
+        /Expected notifications to be:.*"value": "b".*"value": "e".*But got:.*Array \[].*Difference:/s.test(e.message)));
+    });
+
+    it('throws error when actual and expected messages are different', () => {
+      const actual: TestMessages = [
+        ...messages,
+        { frame: 60, notification: { kind: 'C' }}
+      ];
+
+      expect(() => assertDeepEqual(actual, messages)).toThrowError(expect.toSatisfy(e =>
+        /Expected notifications to be:.*But got:.*"kind": "C".*Difference:/s.test(e.message)));
+    });
+
+    it('throws error when actual and expected values are different', () => {
+      const actual: TestMessages = [
+        { frame: 20, notification: { kind: 'N', value: 43 } }
+      ];
+      const expected: TestMessages = [
+        { frame: 20, notification: { kind: 'N', value: 42 } }
+      ];
+
+      expect(() => assertDeepEqual(actual, expected)).toThrowError(expect.toSatisfy(e =>
+        /Expected notifications to be:.*Array \[.*"value": 42.*But got:.*Array \[.*"value": 43.*Difference/s.test(e.message)));
+    });
   });
 });
--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -2,7 +2,7 @@ import { TestScheduler } from 'rxjs/testing';
 import { assertDeepEqual } from './assert-deep-equal';
 
 export class Scheduler {
-  public static instance: TestScheduler | null;
+  public static instance?: TestScheduler;
 
   public static init(): void {
     Scheduler.instance = new TestScheduler(assertDeepEqual);
@@ -16,6 +16,6 @@ export class Scheduler {
   }
 
   public static reset(): void {
-    Scheduler.instance = null;
+    Scheduler.instance = undefined;
   }
 }
